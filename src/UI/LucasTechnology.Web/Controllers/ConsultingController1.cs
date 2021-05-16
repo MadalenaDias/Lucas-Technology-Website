93c589fedc0d7cf98ca8e81de9fa1f458027b35a
LESTER CERIOLI <lcerioli@lucastecnologiaservices.onmicrosoft.com>
2021-05-15 22:39:10 -0300

    "Initial Repository"

---
+++ src/UI/LucasTechnology.Web/Controllers/ConsultingController1.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LucasTechnology.Web.Controllers
+{
+    public class ConsultingController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ src/UI/LucasTechnology.Web/Controllers/PortfolioController1.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LucasTechnology.Web.Controllers
+{
+    public class PortfolioController1 : Controller
+    {
+        // GET: PortfolioController1
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: PortfolioController1/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: PortfolioController1/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: PortfolioController1/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: PortfolioController1/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: PortfolioController1/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: PortfolioController1/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: PortfolioController1/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
